36e45f308c22dcbd9fe53dd50573f43d7e2b69cf
Chris Moylan <chris@chrismoylan.com>
2019-11-23 08:30:24 -0600

    fish funcs2

--- home/.config/fish/functions/vpn.fish
+++ home/.config/fish/functions/vpn.fish
@@ -1,4 +1,10 @@
-function nvpn
-  nordvpn disconnect
-  nordvpn connect
+function vpn
+  if test $argv = 'u'
+    nordvpn connect
+  else if test $argv = 'd'
+    nordvpn disconnect
+  else
+    nordvpn disconnect
+    nordvpn connect
+  end
 end
