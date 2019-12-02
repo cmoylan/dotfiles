3d61ed3d3b48926182eaaacb941f9310416cfec2
Chris Moylan <chris@chrismoylan.com>
2019-12-02 11:48:57 -0600

    tweaking

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -1,10 +1,8 @@
-#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-readtheorg.setup
-
 (setq org-log-done t)
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
 (load "/Users/cmoylan/quicklisp/clhs-use-local.el" t)
 
-(after! treemacs
+(after! treemacs-icons-dired
   (treemacs-icons-dired-mode))
--- home/.doom.d/packages.el
+++ home/.doom.d/packages.el
@@ -6,3 +6,4 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 (package! kaolin-themes)
+(package! treemacs-icons-dired)
