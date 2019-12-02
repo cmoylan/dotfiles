2aa6ebd2897215c90364302ed45f7c5a5b154a6f
Chris Moylan <chris@chrismoylan.com>
2019-12-02 10:44:59 -0600

    going literate

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -1,44 +1,7 @@
-;;; .doom.d/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here
-
-
-
-;; Org mode
 (setq org-log-done t)
 
-
-;; Treemacs config
-;(treemacs-icons-dired-mode)
-
-
-;; Slime
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
 (load "/Users/cmoylan/quicklisp/clhs-use-local.el" t)
 
-
-;;; Centaur Tabs
-;(use-package centaur-tabs
-;  :demand
-;  :config
-;  (centaur-tabs-mode t)
-;  :bind
-;  (:map evil-normal-state-map
-;    ("g t" . centaur-tabs-forward)
-;    ("g T" . centaur-tabs-backward)))
-;
-;(setq centaur-tabs-set-icons t)            ; show icons
-;(setq centaur-tabs-cycle-scope 'tabs)      ; only cycle through tabs in current group
-;;(centaur-tabs-group-by-projectile-project) ; group tabs by projectile project
-;
-;;; hide certain buffers
-;(defun centaur-tabs-hide-tab (x)
-;  (let ((name (format "%s" x)))
-;(or
-; (string-prefix-p "*epc" name)
-; (string-prefix-p "*helm" name)
-; (string-prefix-p "*Compile-Log*" name)
-; (string-prefix-p "*lsp" name)
-; (and (string-prefix-p "magit" name)
-;      (not (file-name-extension name))))))
+;(treemacs-icons-dired-mode)
