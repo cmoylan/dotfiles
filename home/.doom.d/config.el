d368b8827f12d366c84d71832dc4014039ebc6a9
Chris Moylan <chris@chrismoylan.com>
2019-12-02 10:56:25 -0600

    consolidate configs

--- home/.doom.d/config.el
+++ home/.doom.d/config.el
@@ -1,7 +1,10 @@
+#+SETUPFILE: https://fniessen.github.io/org-html-themes/setup/theme-readtheorg.setup
+
 (setq org-log-done t)
 
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
 (load "/Users/cmoylan/quicklisp/clhs-use-local.el" t)
 
-;(treemacs-icons-dired-mode)
+(after! treemacs
+  (treemacs-icons-dired-mode))
