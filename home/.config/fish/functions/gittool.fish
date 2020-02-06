7a301e1f3277a5fc525b91450af7a67af25db5fa
Chris Moylan <chris@chrismoylan.com>
2020-02-06 11:36:33 -0600

    git utilities

--- home/.config/fish/functions/gittool.fish
+++ home/.config/fish/functions/gittool.fish
@@ -1,4 +1,27 @@
-function gittool --d "git utilities"
+function gittool -a cmd -d "git utilities"
+  if test (count $cmd) -gt 0
+    switch "$cmd"
+      case bc
+        git-branch-clean
+      case bs
+        if not isatty
+          while read -l arg
+            set argv $argv $arg
+          end
+        end
+        echo $argv
+    end
+
+  else
+    echo "Available commands:"
+    echo "branch clean - bc"
+    echo "branch save  - bs"
+    read -l -P "Enter a command and arguments: " command
+    gittool $command
+  end
+end
+
+function git-branch-clean --d "clean old git branches"
     git branch | grep -v "master" | grep -v "save"
     read -l -P "Branches to be deleted. Proceed? (y/n) > " proceed
 
